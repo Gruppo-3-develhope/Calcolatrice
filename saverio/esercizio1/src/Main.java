664e3adc68038930f922480c2c78ca775182a851
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-10-25 11:25:55 +0200

    commit

---
+++ saverio/esercizio1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String [] args){
+   int base= 5;
+   int altezza= 10;
+   int area= base * altezza;
+
+   System.out.println("L'area del triangolo è =" + area );
+
+
+
+    }
+}
