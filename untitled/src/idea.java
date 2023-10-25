b02035b57e4ab586d1a09d1f44dffa61244d9687
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-10-25 12:26:14 +0200

    commit2

---
+++ untitled/src/idea.java
@@ -0,0 +1,13 @@
+public class idea {
+    public static void main(String args[]){
+        int altezza= 10;
+        int base= 15;
+        int area= base * altezza / 2;
+        System.out.println("L'area del triangolo è =" + area );
+
+    }
+
+
+
+
+}
