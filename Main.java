06ec4f4e6edea79872a81c16b567d6f18ee9f410
Saveriodinapoli19 <148437018+Saveriodinapoli19@users.noreply.github.com>
2023-10-31 15:30:54 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,15 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Ctrl+1 with your caret at the highlighted text to see how
+        // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        // Press Alt+Maiusc+X or click the green arrow button in the gutter to run the code.
+        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
-            // Press Alt+Maiusc+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+Maiusc+B.
+            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
     }
