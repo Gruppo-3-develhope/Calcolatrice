b557ab866c11bdbcd1449a39440e2e88ba47296a
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-11-03 12:08:51 +0100

    moltiplicazione team3

---
+++ progetto1team3/src/Main.java
@@ -0,0 +1,11 @@
+//Un metodo che preveda il calcolo della moltiplicazione di 2 valori
+// passati in ingresso con il valore risultante restituito
+
+public class Main{
+    public static void main(String[] args){
+        int x=5;
+        int y=10;
+        int z= x*y;
+        System.out.println("Il valore risultante è:" + " " + z);
+    }
+}
