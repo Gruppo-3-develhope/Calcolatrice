7ed9a80f32c7deef9af51a545277a7ebe26c0e32
sunflowerpoet <146355210+sunflowerpoet@users.noreply.github.com>
2023-11-04 12:32:06 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,57 @@
 public class Main {
-    public static void main (String[] agrs){
+    public static void main (String[] args) {
 
+        int x = 10;
+        int y = 2;
+        char calc = '+';
+
+        System.out.println("Input values: " + x + " , " + y);
+        System.out.println("Operation: " + x + " " + calc + " " + y);
+        System.out.println("Type of operation: " + operation(calc));
+
+        Calcolatrice calcolatrice = new Calcolatrice(x, y, calc);
+        calcolatrice.setX(10);
+        calcolatrice.setY(2);
+        calcolatrice.setCalc('+');
+
+        int result = calcolatrice.risolviOperazione();
+        System.out.println("Result: " + result);
+
+        if (isPari(result)) {
+            System.out.println(result + " = even");
+        } else {
+            System.out.println(result + " = odd");
+        }
+
+    }
+
+
+    public static String operation (char calc) {
+
+        String opType;
+        switch (calc) {
+            case '+':
+                opType = "sum";
+                break;
+            case '-':
+                opType = "subtraction";
+                break;
+            case '*':
+                opType = "multiplication";
+                break;
+            case '/':
+                opType = "division";
+                break;
+            case '^' :
+                opType = "power";
+                break;
+            default:
+                opType = "error: invalid char";
+        }
+        return opType;
+    }
+
+    public static boolean isPari (int result) {
+        return result % 2 == 0;
     }
 }
