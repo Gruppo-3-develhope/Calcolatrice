4d816f1945b213704de8c6c965daf06f37beb946
sunflowerpoet <146355210+sunflowerpoet@users.noreply.github.com>
2023-11-06 14:51:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,57 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Ctrl+1 with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main (String[] args) {
 
-        // Press Alt+Maiusc+X or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int x = 10;
+        int y = 2;
+        char calc = '+';
 
-            // Press Alt+Maiusc+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+Maiusc+B.
-            System.out.println("i = " + i);
+        System.out.println("Input values: " + x + " , " + y);
+        System.out.println("Operation: " + x + " " + calc + " " + y);
+        System.out.println("Type of operation: " + operation(calc));
+
+        Calcolatrice calcolatrice = new Calcolatrice(x, y, calc);
+        calcolatrice.setX(10);
+        calcolatrice.setY(2);
+        calcolatrice.setCalc('+');
+
+        int result = calcolatrice.risolviOperazione();
+        System.out.println("Result: " + result);
+
+        if (isPari(result)) {
+            System.out.println(result + " = even");
+        } else {
+            System.out.println(result + " = odd");
         }
+
+    }
+
+
+    public static String operation (char calc) {
+
+        String opType;
+        switch (calc) {
+            case '+':
+                opType = "sum";
+                break;
+            case '-':
+                opType = "subtraction";
+                break;
+            case '*':
+                opType = "multiplication";
+                break;
+            case '/':
+                opType = "division";
+                break;
+            case '^' :
+                opType = "power";
+                break;
+            default:
+                opType = "error: invalid char";
+        }
+        return opType;
+    }
+
+    public static boolean isPari (int result) {
+        return result % 2 == 0;
     }
 }
