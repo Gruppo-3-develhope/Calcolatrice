e03e144c01a7ae9d801ae53f614d5fafdf54192a
sunflowerpoet <146355210+sunflowerpoet@users.noreply.github.com>
2023-11-07 08:38:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,57 +1,57 @@
-public class Main {
-    public static void main (String[] args) {
+public class Main {                                                                // creo la classe Main
+        public static void main (String[] args) {
 
-        int x = 10;
-        int y = 2;
-        char calc = '+';
+            int x = 10;                                                            // imposto due numeri int come operandi
+            int y = 2;
+            char calc = '-';                                                       // scelgo un carattere del simbolo dell'operazione (in questo caso sottrazione)
 
-        System.out.println("Input values: " + x + " , " + y);
-        System.out.println("Operation: " + x + " " + calc + " " + y);
-        System.out.println("Type of operation: " + operation(calc));
+            System.out.println("Input values: " + x + " , " + y);                  // stampo i valori di input
+            System.out.println("Operation: " + x + " " + calc + " " + y);
+            System.out.println("Type of operation: " + operation(calc));
 
-        Calcolatrice calcolatrice = new Calcolatrice(x, y, calc);
-        calcolatrice.setX(10);
-        calcolatrice.setY(2);
-        calcolatrice.setCalc('+');
+            Subtraction subtracion = new Subtraction(x, y, calc);                  // costruttore classe somma
+            subtracion.setX(10);
+            subtracion.setY(2);
+            subtracion.setCalc('-');
 
-        int result = calcolatrice.risolviOperazione();
-        System.out.println("Result: " + result);
+            int result = sum.solveOperation();                                     // soluzione e stampo il risultato in terminale
+            System.out.println("Result: " + result);
 
-        if (isPari(result)) {
-            System.out.println(result + " = even");
-        } else {
-            System.out.println(result + " = odd");
+            if (isEven(result)) {                                                  // controllo se il risultato e' pari o dipari e lo stampo in terminale
+                System.out.println(result + " = even");
+            } else {
+                System.out.println(result + " = odd");
+            }
+
         }
 
-    }
 
-
-    public static String operation (char calc) {
-
-        String opType;
-        switch (calc) {
-            case '+':
-                opType = "sum";
-                break;
-            case '-':
-                opType = "subtraction";
-                break;
-            case '*':
-                opType = "multiplication";
-                break;
-            case '/':
-                opType = "division";
-                break;
-            case '^' :
-                opType = "power";
-                break;
-            default:
-                opType = "error: invalid char";
+        public static String operation (char calc) {                                // metodo per conrollare se l'operazione e' una somma, sottrazione, 
+                                                                                    // moltiplicazione, divisione o potenza
+            String opType;
+            switch (calc) {
+                case '+':
+                    opType = "sum";
+                    break;
+                case '-':
+                    opType = "subtraction";
+                    break;
+                case '*':
+                    opType = "multiplication";
+                    break;
+                case '/':
+                    opType = "division";
+                    break;
+                case '^' :
+                    opType = "power";
+                    break;
+                default:
+                    opType = "error: invalid char";
+            }
+            return opType;
         }
-        return opType;
-    }
 
-    public static boolean isPari (int result) {
-        return result % 2 == 0;
-    }
+        public static boolean isEven (int result) {                                // metodo per controllare se il risultato e' pari o dispari
+            return result % 2 == 0;
+        }
 }
