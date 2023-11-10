fee9011669c96972ae3b6e349557b929d7ae0ef0
MargheritaDGit <147701140+MargheritaDGit@users.noreply.github.com>
2023-11-10 11:39:43 +0100

    Update Subtraction.java
    Signed-off-by: MargheritaDGit <147701140+MargheritaDGit@users.noreply.github.com>

--- Subtraction.java
+++ Subtraction.java
@@ -1,36 +1,16 @@
-public class Subtraction {                                       // creo la classe Subtraction
-    private int x;                                               // imposto le variabili degli operatori e del carattere
-    private int y;
+public class Subtraction extends Calcolatrice {                                       // creo la calsse Power
+    private double x;                                         // imposto le variabili degli operatori e del carattere
+    private double y;
     private char calc;
 
-    public Subtraction(int x, int y, char calc) {                // creo l'oggetto Subtraction, getter e setter delle variabili
+    public Subtraction (double x, double y, char calc) {               // creo l'oggetto Power, getter e setter delle variabili
         this.x = x;
         this.y = y;
         this.calc = calc;
     }
 
-    public int getX() {
-        return x;
-    }
-    public void setX(int newX) {
-        this.x = newX;
-    }
-
-    public int getY () {
-        return x;
-    }
-    public void setY (int newY) {
-        this.y = newY;
-    }
-
-    public char getCalc () {
-        return calc;
-    }
-    public void setCalc (char newCalc) {
-        this.calc = newCalc;
-    }
-
-    public int solveOperation() {                                                 // risolvo l'operazione
+    @Override
+    public double solveOperation() {                                                 // risolvo l'operazione
         int result = 0;
         switch (calc) {
             case '+':
