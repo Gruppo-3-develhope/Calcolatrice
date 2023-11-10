ca61a1268d5d3d4a1539597dfe8afd7df5389816
MargheritaDGit <147701140+MargheritaDGit@users.noreply.github.com>
2023-11-10 11:55:27 +0100

    Create Subtraction.java
    Signed-off-by: MargheritaDGit <147701140+MargheritaDGit@users.noreply.github.com>

---
+++ src/Subtraction.java
@@ -0,0 +1,40 @@
+public class Subtraction extends Calcolatrice {                                       // creo la calsse Power
+    private double x;                                         // imposto le variabili degli operatori e del carattere
+    private double y;
+    private char calc;
+
+    public Subtraction (double x, double y, char calc) {               // creo l'oggetto Power, getter e setter delle variabili
+        this.x = x;
+        this.y = y;
+        this.calc = calc;
+    }
+
+    @Override
+    public double solveOperation() {                                                 // risolvo l'operazione
+        int result = 0;
+        switch (calc) {
+            case '+':
+                result = x + y;
+                break;
+            case '-':
+                result = x - y;
+                break;
+            case '*':
+                result = x * y;
+                break;
+            case '/':
+                if (y != 0) {
+                    result = x / y;
+                } else {
+                    System.out.println("Impossible to divide by zero.");
+                }
+                break;
+            case '^':
+                result = x ^ y;
+                break;
+            default:
+                System.out.println("Invaild operation.");
+        }
+        return result;
+    }
+}
